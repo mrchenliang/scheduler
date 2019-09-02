8696aaeb37822f74cad39f090fd566779b4e650e
mrchenliang <c.liang@ualberta.ca>
2019-09-02 13:26:31 -0400

    listing days

---
+++ src/components/DayListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classnames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", { 
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <div className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h1>{props.name}</h1>
+      {props.spots === 0 ? "no" : props.spots} spot
+      {props.spots === 1 ? "" : "s"} remaining
+    </div>
+  );
+}
