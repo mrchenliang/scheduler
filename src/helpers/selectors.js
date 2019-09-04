5282708684288cfdc2ec3441c1baafb3ca5c2af4
mrchenliang <c.liang@ualberta.ca>
2019-09-04 17:33:08 -0400

    adding all test cases

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -32,18 +32,18 @@ export function getInterview(state, interview) {
 }
 
 export function getInterviewersForDay(state, day) {
-  let result = [];
+  let ans = [];
   for (let i = 0; i < state.days.length; i++) {
     if (state.days[i].name === day) {
-      result = [...state.days[i].interviewers];
+      ans = state.days[i].interviewers;
     }
   }
   for (const interviewer in state.interviewers) {
-    for (let i = 0; i < result.length; i++) {
-      if (result[i] === state.appointments[interviewers].id) {
-        result[i] = state.appointments[interviewers];
+    for (let i = 0; i < ans.length; i++) {
+      if (ans[i] === Number(interviewer)) {
+        ans[i] = state.interviewers[String(interviewer)];
       }
     }
   }
-  return result;
+  return ans;
 }
