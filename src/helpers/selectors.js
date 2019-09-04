b8371d19d62794328f126e0e424482a0d587f4be
mrchenliang <c.liang@ualberta.ca>
2019-09-04 17:05:33 -0400

    minor changes to helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,20 +1,18 @@
-
-
 export function getAppointmentsForDay(state, day) {
-  let ans = [];
+  let result = [];
   for (let i = 0; i < state.days.length; i++) {
     if (state.days[i].name === day) {
-      ans = state.days[i].appointments;
+      result = state.days[i].appointments;
     }
   }
   for (const appointment in state.appointments) {
-    for (let i=0;i<ans.length;i++){
-      if(ans[i]===state.appointments[appointment].id){
-        ans[i]=state.appointments[appointment];
+    for (let i = 0; i < result.length; i++){
+      if(result[i]===state.appointments[appointment].id) {
+        result[i]=state.appointments[appointment];
       }
     }
   }
-  return ans;
+  return result;
 } 
 
 export function getInterview(state, interview) {
@@ -22,12 +20,12 @@ export function getInterview(state, interview) {
     return null;
   }
   let id = interview.interviewer;
-  let studentName = interview.student;
+  let student = interview.student;
   for (const interviewer in state.interviewers) {
     if (id === state.interviewers[interviewer].id) {
       return {
         interviewer: state.interviewers[interviewer], 
-        student: studentName
+        student: student
       };
     }
   }
