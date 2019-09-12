32523c37a2ca0cb65e74e844a4183a7c5d65d405
mrchenliang <c.liang@ualberta.ca>
2019-09-12 15:50:14 -0400

    minor fixes

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -33,8 +33,6 @@ describe("Appointments", () => {
       .click();
 
     cy.get("[alt='Edit']").click({ force: true });
-
-    cy.get("[data-testid=student-name-input]").type("hans");
     cy.get('[alt="Sylvia Palmer"]').click();
     cy.contains("Save").click();
     cy.contains(".appointment__card--show", "Sylvia Palmer");
