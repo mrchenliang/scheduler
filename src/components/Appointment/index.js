e5b2830d5aa8ddfdcc0560fa0d416b026d1f0b6a
mrchenliang <c.liang@ualberta.ca>
2019-09-11 23:10:57 -0400

    fixing test cases for applicationa and appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -44,7 +44,7 @@ const Appointment = props => {
 
 
   return (
-    <article className="appointment">
+    <article data-testid="appointment" className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && props.interview && (
