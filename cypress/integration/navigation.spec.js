0d9c36575b1b9f37c000e843de060276f73ff86a
mrchenliang <c.liang@ualberta.ca>
2019-09-20 00:03:41 -0400

    adding cypress testing and finalizing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,49 +1,13 @@
+  
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
-  }); 
-});
+  });
 
-describe("Appointments", () => {
-  beforeEach(() => {
-    cy.request("POST", "/api/debug/reset");
+  it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.contains("Monday");
-  });
-  it("should book an interview", () => {
-    cy.get("[alt=Add]")
-      .first()
-      .click();
-    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    cy.get('[alt="Sylvia Palmer"]').click();
-    cy.contains("Save").click();
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
-  });
-
-  it("should edit an interview", () => {
-    cy.get("[class=text--regular]")
-      .contains("Archie Cohen")
-      .click();
-
-    cy.get("[alt='Edit']").click({ force: true });
-    cy.get('[alt="Sylvia Palmer"]').click();
-    cy.contains("Save").click();
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
-  });
-
-  it("should cancel an interview", () => {
-    cy.get("[class=text--regular]")
-      .contains("Archie Cohen")
-      .click();
-
-    cy.get("[alt='Delete']").click({ force: true });
-    cy.contains("Confirm").click();
-
-    cy.contains("DELETING").should("exist");
-
-    cy.contains("DELETING").should("not.exist");
-
-    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
